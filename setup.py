3d8d15e61ca367d0dc958a438b0bdeeb5576676b
Sanket G Shanbhag <sanketshanbhag1349@gmail.com>
2025-03-07 15:53:27 +0530

    update

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Medical Chatbot',
+    version= '0.0.0',
+    author= 'Sanket Shanbhag',
+    author_email= 'sanketshanbhag1349@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
